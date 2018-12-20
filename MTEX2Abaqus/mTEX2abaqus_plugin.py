6e5c1cca9a0733e9a1c80500503d2d009c89f22d
Dorian Depriester <dorian@crans.org>
2018-12-20 10:48:12 +0100

    Add files via upload

---
+++ MTEX2Abaqus/mTEX2abaqus_plugin.py
@@ -0,0 +1,19 @@
+from abaqusGui import getAFXApp, Activator, AFXMode
+from abaqusConstants import ALL
+import os
+thisPath = os.path.abspath(__file__)
+thisDir = os.path.dirname(thisPath)
+
+toolset = getAFXApp().getAFXMainWindow().getPluginToolset()
+toolset.registerGuiMenuButton(
+    buttonText='MTEX2abaqus', 
+    object=Activator(os.path.join(thisDir, 'mTEX2abaqusDB.py')),
+    kernelInitString='import AbaqusImport',
+    messageId=AFXMode.ID_ACTIVATE,
+    icon=None,
+    applicableModules=ALL,
+    version='N/A',
+    author='N/A',
+    description='N/A',
+    helpUrl='N/A'
+)
