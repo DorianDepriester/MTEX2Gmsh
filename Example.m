3798bb585d1ef284e4fe7591dc96259f46573c3a
Dorian Depriester <dorian@crans.org>
2019-05-16 16:37:08 +0200

    remove unnecessary lines in Example.m

--- Example.m
+++ Example.m
@@ -1,22 +1,14 @@
-clear all
-close all
-
 %% MTEX stuff
 plotx2east
 mtexdata small
 ebsd = ebsd('indexed');									% Remove unindexed points
 [grains,ebsd.grainId,ebsd.mis2mean] = calcGrains(ebsd);	% Compute grains
 
-% region=[33800 5266 300 300];
-% condition = inpolygon(ebsd,region);
-% %ebsd = ebsd(condition)
-
 selected_grains = grains(grains.grainSize > 1);			% Outlier removal
 ebsd = ebsd(selected_grains);
 [grains,ebsd.grainId,ebsd.mis2mean] = calcGrains(ebsd);	% Update grains
 
 %% gmshGeo stuff
-%grains=grains([4 6]);
 G=gmshGeo(grains);	% Format data structure
 
 figure
@@ -25,7 +17,7 @@ plot(G);			% Plot surfaces
 %% Generate Gmsh-readable file and export the mesh
 savegeo(G,'small.geo','thickness',50,'elementType','Brick');
 
-% Export grain properties
+%% Export grain properties
 exportGrainProps(G,'small.csv');
 Gmsh('small.geo','inp')
 
