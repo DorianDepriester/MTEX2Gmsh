560ee79aa12e41a702bf659f65bca33020b176a0
Dorian Depriester <dorian@crans.org>
2020-02-11 10:02:01 +0100

    Simplified version for script twins.m

--- docs/Examples/twins.m
+++ docs/Examples/twins.m
@@ -1,6 +1,5 @@
 mtexdata twins
-ebsd=ebsd('indexed')
-grains=calcGrains(ebsd)
+ebsd=ebsd('indexed');
+grains=calcGrains(ebsd);
 G=gmshGeo(cond_smooth(grains));
-G=simplify(G);
 mesh(G,'twins.msh','ElementSize',0.2,'gradient',0.5,'elementType','Brick');
