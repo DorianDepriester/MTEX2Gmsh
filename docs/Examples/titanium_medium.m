d455a55b275e644decad6e1dd4e2409fa2f4c6f4
Dorian Depriester <dorian@crans.org>
2020-02-24 21:38:22 +0100

    change savegeo into mesh

--- docs/Examples/titanium_medium.m
+++ docs/Examples/titanium_medium.m
@@ -2,4 +2,4 @@ mtexdata titanium
 grains=calcGrains(ebsd);
 grains=cond_smooth(grains);
 G=gmshGeo(grains);
-savegeo(G,'titanium.geo','ElementSize',10,'gradient',5,'ElementType','Tri');
+mesh(G,'titanium.geo','ElementSize',10,'gradient',5,'ElementType','Tri');
