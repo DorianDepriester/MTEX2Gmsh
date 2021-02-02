8a1532120c6f13f9af41bc65f5bdba5714875296
Dorian Depriester <dorian@crans.org>
2021-02-02 15:06:03 +0100

    Fixed out of bounds indices because of wrong condition

--- docs/Examples/aachen.m
+++ docs/Examples/aachen.m
@@ -2,5 +2,5 @@ mtexdata aachen
 ebsd=ebsd('indexed');
 grains=calcGrains(ebsd);
 G=gmshGeo(grains);
-G=simplify(G);
-mesh(G,'aachen.msh','ElementSize',0.7,'gradient',0.2)
+%G=simplify(G);
+%mesh(G,'aachen.msh','ElementSize',0.7,'gradient',0.2)
