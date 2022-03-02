f47dac029b5f30d3e4f209e1e6bafb935917b3fa
Dorian Depriester <dorian@crans.org>
2022-03-02 12:22:57 +0100

    Use 'Distance' instead of 'Attractor'

--- @gmshGeo/private/addAttractorField.m
+++ @gmshGeo/private/addAttractorField.m
@@ -1,5 +1,5 @@
 function id_field = addAttractorField(ffid, id_field, n_segments, def_size, slope)
-	fprintf(ffid,'Field[%i] = Attractor;\n',id_field);
+	fprintf(ffid,'Field[%i] = Distance;\n',id_field);
 	fprintf(ffid,'Field[%i].EdgesList ={1:%i};\n',id_field,n_segments);
 	id_field=id_field+1;
 	fprintf(ffid,'Field[%i] = MathEval;\n',id_field);
