b837329088ee4f816a12494b3a3e9a34d19375a3
Dorian Depriester <dorian@crans.org>
2022-04-13 09:25:50 +0200

    Change wrong format and missing medium in command Update image (with OCC kernel)

--- docs/Examples/titanium_medium.m
+++ docs/Examples/titanium_medium.m
@@ -2,4 +2,4 @@ mtexdata titanium
 grains=calcGrains(ebsd);
 grains=cond_smooth(grains);
 G=gmshGeo(grains);
-mesh(G,'titanium.geo','ElementSize',10,'gradient',5,'ElementType','Tri');
+mesh(G,'titanium-medium.msh','ElementSize',20,'medium',[2000 2000 200]);
